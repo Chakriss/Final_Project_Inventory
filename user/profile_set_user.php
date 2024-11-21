fb3f0c0ba42b0b46d858040b07d128616eacb1a1
Chakriss <Chanori40@gmail.com>
2024-11-21 14:55:45 +0700

    update

--- user/profile_set_user.php
+++ user/profile_set_user.php
@@ -95,8 +95,8 @@ $result_dept = selectDept($conn);
     <div class="modal fade" id="resetPasswordModal" tabindex="-1" aria-labelledby="resetPasswordModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="resetPasswordModalLabel">Reset Password</h5>
+                <div class="modal-header bg-warning">
+                    <h5 class="modal-title white" id="resetPasswordModalLabel">Reset Password</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
